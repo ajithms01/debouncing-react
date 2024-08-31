82a9491c1690249b52759af9b26cc36e2a948594
Ajith Sureshkumar(UST <284702@ust.com>
2024-08-31 15:30:15 +0530

    react app with debounce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const[data,setData]=useState([]);
+  useEffect(()=>{
+    async ()=>{
+      const result=await axios.get('./jsonfile');
+      setData(result);
+    }
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input
+        placeholder="Search Input.."
+        onChange={(event) => setPinCode(event.target.value)}
+      />
     </div>
   );
 }
-
-export default App;
